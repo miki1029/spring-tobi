41a18c80f3015f0e50be80a1d7691d1b9884397b
miki1029 <miki1029g@gmail.com>
2015-10-23 15:46:25 +0900

    UserDao에 싱글턴 스코프, DI, DL 적용

--- user/src/main/java/springbook/user/dao/UserDao.java
+++ user/src/main/java/springbook/user/dao/UserDao.java
@@ -1,5 +1,6 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.*;
@@ -9,8 +10,18 @@ import java.sql.*;
  */
 public class UserDao {
     // 인터페이스를 통해 오브젝트에 접근
-    private ConnectionMaker connectionMaker;
+    // 싱글턴 스코프를 갖는 객체는 인스턴스 상태를 두지 않는다. 하지만 읽기 전용인 final은 무관하다.
+    private final ConnectionMaker connectionMaker;
 
+    // DL : Dependency Lookup 의존관계 검색, UserDao가 Bean일 필요가 없다.
+    public UserDao() {
+//        DaoFactory daoFactory = new DaoFactory();
+//        this.connectionMaker = daoFactory.connectionMaker();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        this.connectionMaker = context.getBean("connectionMaker", ConnectionMaker.class);
+    }
+
+    // DI : 의존 관계 주입(설정), 사용 의존 관계, 스프링에 의해 DI 받으려면 UserDao가 Bean이어야 한다.
     public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
