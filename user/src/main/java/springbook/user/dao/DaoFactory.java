3a2e1145c567199a7a3be2bec9d20fd306a209ac
miki1029 <miki1029g@gmail.com>
2015-10-23 13:16:13 +0900

    spring 애플리케이션 컨텍스트 적용

--- user/src/main/java/springbook/user/dao/DaoFactory.java
+++ user/src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,21 +1,19 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by miki on 15. 10. 23..
  */
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
-    public AccountDao accountDao() {
-        return new AccountDao(connectionMaker());
-    }
-
-    public MessageDao messageDao() {
-        return new MessageDao(connectionMaker());
-    }
-
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
--- user/src/test/java/springbook/user/dao/UserDaoTest.java
+++ user/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,5 +1,7 @@
 package springbook.user.dao;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
@@ -10,7 +12,8 @@ import java.sql.SQLException;
 public class UserDaoTest {
     // 런타임 오브젝트 관계를 맺어주는 것이 클라이언트의 책임
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User("miki", "김민우", "1212");
         dao.add(user);
