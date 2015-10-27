a324ae4d627c3739cee67d62d5b143cb6c4b6d72
miki1029 <miki1029g@gmail.com>
2015-10-27 21:57:51 +0900

    test-applicationContext.xml을 이용한 리팩토링

--- user/src/test/java/springbook/user/dao/UserDaoTest.java
+++ user/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -6,13 +6,10 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -22,8 +19,7 @@ import static org.junit.Assert.assertThat;
  * Created by miki on 15. 10. 17..
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "/applicationContext.xml")
-@DirtiesContext // 테스트 메소드에서 애플리케이션 컨텍스트의 구성이나 상태를 변경한다는 것을 알리고 컨텍스트를 재사용(공유)하지 않음
+@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
     @Autowired
     UserDao dao;
@@ -38,14 +34,6 @@ public class UserDaoTest {
         user1 = new User("gyumee", "박성철", "springno1");
         user2 = new User("leegw700", "이길원", "springno2");
         user3 = new User("bumjin", "박범진", "springno3");
-
-        DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/testbook?useUnicode=true&characterEncoding=utf8", "spring", "book", true);
-/*        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(org.hsqldb.jdbc.JDBCDriver.class);
-        dataSource.setUrl("jdbc:hsqldb:mem:testdb");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");*/
-        dao.setDataSource(dataSource); // 코드에 의한 수동 DI
     }
 
     @Test
