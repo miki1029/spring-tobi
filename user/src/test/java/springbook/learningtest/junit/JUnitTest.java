07447c31bec0a28620d012a41c51fa2437359190
miki1029 <miki1029g@gmail.com>
2015-10-29 15:28:57 +0900

    직전 테스트에서 만들어진 오브젝트와만 비교하는 문제점 수정

--- user/src/test/java/springbook/learningtest/junit/JUnitTest.java
+++ user/src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -2,29 +2,30 @@ package springbook.learningtest.junit;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 /**
  * Created by miki on 15. 10. 29..
  */
 public class JUnitTest {
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 
     @Test public void test1() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test public void test2() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test public void test3() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 }
