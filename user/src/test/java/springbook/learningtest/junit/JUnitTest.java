3fd857642c7d180f9c5eb0cfbeb76f1ac890467d
miki1029 <miki1029g@gmail.com>
2015-10-29 15:39:06 +0900

    spring context(junit.xml)가 정말 전체 테스트에서 공유하는지 테스트

--- user/src/test/java/springbook/learningtest/junit/JUnitTest.java
+++ user/src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -1,6 +1,11 @@
 package springbook.learningtest.junit;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,21 +16,35 @@ import static org.junit.Assert.assertThat;
 /**
  * Created by miki on 15. 10. 29..
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/junit.xml")
 public class JUnitTest {
+    @Autowired ApplicationContext context;
+
     static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+    static ApplicationContext contextObject = null;
 
     @Test public void test1() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test public void test2() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test public void test3() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 }
